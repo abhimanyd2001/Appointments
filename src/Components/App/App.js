8eccbb6b04018c42672720de62fda1c5b5c9632f
abhimanyd2001 <abhimanyd2001@gmail.com>
2020-06-30 17:07:40 -0400

    Added sub components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+import AddAppointments from '../AddAppointments/AddAppointments';
+import SearchAppointments from '../SearchAppointments/SearchAppointments';
+import ListAppointments from '../ListAppointments/ListAppointments';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myName: 'Abhimanyu'
+    }
+  }
+  render() {
+    return (
+
     <main class="page bg-white" id="petratings">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 bg-white">
-          <div class="container">
-            <div>Add Appointments</div>
-            <div>Search Appointments</div>
-            <div>List Appointments</div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 bg-white">
+            <div class="container">
+
+              <AddAppointments />
+              <SearchAppointments />
+              <ListAppointments />
+
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </main>
-  );
+    </main>     
+      
+    )
+  }
 }
 
 export default App;
