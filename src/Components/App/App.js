260fda5fdb7a6b3cd9a0f6e3547575dfef715cc0
abhimanyd2001 <abhimanyd2001@gmail.com>
2020-07-01 15:48:04 -0400

    Added AddAppointments logic

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,26 +5,60 @@ import AddAppointments from '../AddAppointments/AddAppointments';
 import SearchAppointments from '../SearchAppointments/SearchAppointments';
 import ListAppointments from '../ListAppointments/ListAppointments';
 
+import { without } from 'lodash';
+
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      myName: 'Abhimanyu'
+      myAppointments: [],
+      lastId: 0,
+      showDisplay: false
     }
+
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleShow = this.toggleShow.bind(this);
   }
+
+  componentDidMount() {
+    fetch('./appointment_data.json')
+      .then(response => response.json())
+      .then(jsonResponse => {
+        const appointments = jsonResponse.map(item => {
+          item.aptId = this.state.lastId;
+          this.setState({lastId: this.state.lastId + 1})
+          return item;
+        })
+        this.setState({myAppointments: appointments});
+      });
+
+  }
+
+  deleteAppointment(item) {
+    let appts = this.state.myAppointments;
+    appts = without(appts, item);
+    console.log(appts);
+    this.setState({myAppointments: appts});
+  }
+
+  toggleShow() {
+    const oppositeDisplay = !this.state.showDisplay;
+    this.setState({showDisplay: oppositeDisplay});
+  }
+
   render() {
     return (
 
-    <main class="page bg-white" id="petratings">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 bg-white">
-            <div class="container">
+    <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
 
-              <AddAppointments />
+              <AddAppointments toggleShow={this.toggleShow} show={this.state.showDisplay} />
               <SearchAppointments />
-              <ListAppointments />
+              <ListAppointments appointments={this.state.myAppointments} deleteAppointment={this.deleteAppointment} />
 
             </div>
           </div>
